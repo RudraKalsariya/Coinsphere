e5ffddb4d0ec35cfb8ae750c8aee3732e69f2fec
Rudra Kalsariya <127958623+RudraKalsariya@users.noreply.github.com>
2025-11-23 23:17:13 +0530

    Add files via upload

---
+++ Frontend/tailwind.config.cjs
@@ -0,0 +1,12 @@
+// tailwind.config.cjs
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#6055F3',
+      },
+    },
+  },
+  plugins: [],
+}
